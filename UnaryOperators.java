84bb643b46cea7057b4b6307c7683430e53982a4
Kapilrajput089 <kapil335286@gmail.com>
2024-06-10 15:29:33 +0530

    My first commit of java programming with kapil

---
+++ UnaryOperators.java
@@ -0,0 +1,17 @@
+// write a program to use Unary operator in java with kapilcoder.
+ public class UnaryOperators{
+    public static void main(String[] args) {
+        int x=20;
+        ++x; //pre Increment Operator 
+        System.out.println("value of x : = "+x);
+        x++; // post Increment Operator 
+        System.out.println("value of x : = "+x);
+        --x; //pre decrement operator
+        System.out.println("value of x : = "+x);
+        x--; // post decrement operator
+        System.out.println("value of x : = "+x);
+
+
+
+    }
+}
