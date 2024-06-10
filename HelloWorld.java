6c61118acf7f01c5ef35924b962e37ab1af06a47
Kapilrajput089 <kapil335286@gmail.com>
2024-06-10 15:01:01 +0530

    My first commit of java programming with kapil

---
+++ HelloWorld.java
@@ -0,0 +1,8 @@
+// write a program  to print Hello World in java programming -------------with kapilcoder.
+public class HelloWorld{
+    //
+    public static void main(String[] args)
+    {
+        System.out.println("Hello world!");
+    }
+}
