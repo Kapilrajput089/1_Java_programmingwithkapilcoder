8f12e51b584d8206bd01944d29edd10782d2778f
Kapilrajput089 <kapil335286@gmail.com>
2024-06-17 16:26:51 +0530

    My first commit of java programming with kapil

---
+++ ObjExample.java
@@ -0,0 +1,27 @@
+// Write a program to create Box Class with member variable lenght,breadth,height.
+class Box{
+    private int lenght,breadth,height;
+    public void setDimension(int l,int b,int h)
+    {
+        lenght=l;
+        breadth=b;
+        height=h;
+
+    }
+    public void showData()
+    {
+        System.out.println("lenght : = "+lenght);
+          System.out.println("breadth  : = "+breadth);
+            System.out.println("height : = "+height);
+    }
+}
+public class ObjExample{
+    public static void main (String []args)
+    {
+        Box b1 = new Box();
+        b1.setDimension(10,20,30);
+        b1.showData();
+        Box b2 = new Box();
+       // b2.showData();
+    }
+}
--- tempCodeRunnerFile.java
+++ tempCodeRunnerFile.java
@@ -1,1 +1,8 @@
-// unary operators are :-(+,-,++,--).
+public class Intreest{
+    public static void main(String [] args){
+        int r=3,p=10000,t=2,si;
+        si=r*p*t/100;
+    System.out.println("simple "+si);
+
+    }
+}
