b4631ac92ae1d23df38db4757b60630c8e767978
Kapilrajput089 <kapil335286@gmail.com>
2024-07-02 19:31:41 +0530

    My first commit of java progrmming with kapilcoder

---
+++ StringExample.java
@@ -0,0 +1,9 @@
+public  class StringExample{
+    public static void main(String[] args) {
+        String  str1="kapil kumar";
+        String  str2="Deepesh";
+        String  str5="kumar";
+        String  str6=str2+str5;
+        System.out.println("value is "+(str1==str2));
+    }
+}
