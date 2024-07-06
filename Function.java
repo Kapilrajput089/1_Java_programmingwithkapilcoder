f9d769e24e7ad56ae826bb5ef698027570592278
Kapilrajput089 <kapil335286@gmail.com>
2024-07-06 21:32:35 +0530

    My first commit  of java programming with kapil.

--- Function.java
+++ Function.java
@@ -2,7 +2,25 @@
 import java.util.*;
 
 public class Function{
-    public static int  calculatrSum(int a,int b)
+    public static int  calculatrSum(int a,int b) 
+                                        /*
+                               * what is function in java.
+                               * A function is a block of code that performs a specific task.
+                               * *if same functionality is perforemed at multiple places in software then rather then writing the 
+                               * same code agian and agian we create a function and call it every where This helps reduce code redundancy.
+                                    ** Function  Tenchnical treams **
+                                    * Function Definition.
+                                    * Function Declaration.
+                                    * Function Call. 
+                                     
+                    
+                                    *** Benefits of function ***
+
+                                      Modularization.
+                                      Easy to read.
+                                      Easy to debug.
+                                      Easy to modify. 
+                                        */
     
     {
         int sum = a + b;
@@ -19,3 +37,11 @@ public class Function{
     }
 
 }
+/*  
+       ***Ways to difine a function***
+          * Take nothing, Return nothing,
+          * Take somthing, Return nothing,
+          * Take nothing, Return somthing,
+          * Take somthing, Return somthing,
+
+*/
